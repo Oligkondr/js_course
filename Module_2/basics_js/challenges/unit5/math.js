8b5507d223f59eb11ae8198aaa9e8d3d861e0619
Oligkondr <oligkondr@mail.ru>
2023-10-12 15:45:56 +0300

    done challenges/unit5

---
+++ Module_2/basics_js/challenges/unit5/math.js
@@ -0,0 +1,7 @@
+import {default as cube} from './numbers1.js'
+export default cube()
+export {pow2 as pow} from './numbers2.js'
+export {sum2 as sum} from './numbers2.js'
+export {sub2 as sub} from './numbers2.js'
+export {default as multi} from './numbers2.js'
+export {default as sqrt} from './numbers3.js'
