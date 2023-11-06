51a057ce0ad81062e6492efc5be865d9b9d40afa
Oligkondr <oligkon@gmail.com>
2023-11-06 16:10:41 +0300

    Done unit16

---
+++ Module_2/js_arrays/lessons/unit16/test.js
@@ -0,0 +1,5 @@
+import bubbleSort from "./unit16.js";
+
+console.log(bubbleSort([]));
+console.log(bubbleSort([10, 1, 3]));
+console.log(bubbleSort([0, 4, 0, 10, -3]));
--- Module_2/js_arrays/lessons/unit16/unit16.js
+++ Module_2/js_arrays/lessons/unit16/unit16.js
@@ -1,5 +1,19 @@
 const bubbleSort = (array) => {
-
+    let steps = array.length - 1;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i <steps; i += 1) {
+            if (array[i] > array[i + 1]) {
+                const temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = true;
+            }
+        }
+        steps -= 1;
+    } while (swapped);
+    return array;
 };
 
 export default bubbleSort;
