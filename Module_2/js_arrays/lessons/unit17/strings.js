d57dc48018046bb45f45d9f48a989ea8c5d822ac
Oligkondr <oligkon@gmail.com>
2023-11-09 09:50:16 +0300

    Done unit17

--- Module_2/js_arrays/lessons/unit17/strings.js
+++ Module_2/js_arrays/lessons/unit17/strings.js
@@ -2,6 +2,20 @@ const openingSymbols = ['(', '[', '{', '<'];
 const closingSymbols = [')', ']', '}', '>'];
 
 const isBracketStructureBalanced = (text) => {
-
+    let ans = true;
+    const stack = [];
+    for (const element of text) {
+        if (openingSymbols.includes(element)) {
+            stack.push(element);
+        } else if (closingSymbols.includes(element)) {
+            const open = stack.pop();
+            if (element === ')' && open !== '(') ans = false;
+            else if (element === ']' && open !== '[') ans = false;
+            else if (element === '}' && open !== '{') ans = false;
+            else if (element === '>' && open !== '<') ans = false;
+        }
+    }
+    if (stack.length !== 0) ans = false;
+    return ans;
 };
 export default isBracketStructureBalanced;
